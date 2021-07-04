0d9472d042db8714bebc85b2c4774adeed3d008b
Ashita707 <54110837+Ashita707@users.noreply.github.com>
2021-07-04 19:52:16 +0530

    Update Notifications.jsx

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
 import { Button } from '@material-ui/core';
 
-import { SocketContext } from '../Context';
+import { SocketContext } from '../SocketContext';
 
 const Notifications = () => {
   const { answerCall, call, callAccepted } = useContext(SocketContext);
 
   return (
-    <>
+    <div>
       {call.isReceivingCall && !callAccepted && (
         <div style={{ display: 'flex', justifyContent: 'space-around' }}>
           <h1>{call.name} is calling:</h1>
@@ -16,7 +16,7 @@ const Notifications = () => {
           </Button>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
